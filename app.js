97c40ee947a80601bfcc88cad849b5181ac40646
lloveland2 <lloveland2@ivytech.edu>
2023-12-05 23:51:23 -0500

    started m06 tutorial

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://netninja:test12345@cluster01.v6predi.mongodb.net/?retryWrites=true&w=majority'
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000);
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur.'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur.'},
+        {title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur.'},
+    ];
+    res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' })
+});
